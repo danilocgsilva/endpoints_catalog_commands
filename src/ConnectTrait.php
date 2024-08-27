a3ed3cf3d33de593b52b1891a47001fbdd4f2439
Danilo Carlos <danilo.goes_leega@totalexpress.com.br>
2024-08-27 20:44:41 +0000

    wip: updating migration schema

--- src/ConnectTrait.php
+++ src/ConnectTrait.php
@@ -4,18 +4,21 @@ declare(strict_types=1);
 
 namespace Danilocgsilva\EndpointsCatalogCommands;
 
-use Danilocgsilva\EndpointsCatalog\Migrations;
+use Danilocgsilva\EndpointsCatalog\Migrations\MigrationInterface;
+use Danilocgsilva\EndpointsCatalog\Migrations\MigrationManager;
 use PDO;
 
 trait ConnectTrait
 {
-    protected Migrations $migrations;
+    protected MigrationInterface $migrations;
 
     protected PDO $pdo;
 
     protected function connect()
     {
-        $this->migrations = new Migrations();
+        $migrationManager = new MigrationManager();
+        
+        $this->migrations = new $migrationManager->getNextMigrationClass();
         $this->pdo = new PDO(
             sprintf("mysql:host=%s;dbname=%s", getenv('DB_ENDPOINTSCATALOG_HOST'), getenv('DB_ENDPOINTSCATALOG_NAME')),
             getenv('DB_ENDPOINTSCATALOG_USER'),
