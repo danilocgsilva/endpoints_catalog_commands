df2fceea7d63bca10ce3ee705bbbab6e44e92a8e
Danilo Carlos <danilo.goes_leega@totalexpress.com.br>
2024-09-05 13:48:39 +0000

    Updating management for migration manager

--- src/ConnectTrait.php
+++ src/ConnectTrait.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Danilocgsilva\EndpointsCatalogCommands;
 
 use Danilocgsilva\EndpointsCatalog\Migrations\MigrationInterface;
-use Danilocgsilva\EndpointsCatalog\Migrations\MigrationManager;
+use Danilocgsilva\EndpointsCatalog\Migrations\Manager;
 use PDO;
 
 trait ConnectTrait
@@ -14,28 +14,33 @@ trait ConnectTrait
 
     protected PDO $pdo;
 
+    private string $databaseName;
+
     protected function connectMigrate(): void
     {
         $this->connect();
-        $migrationManager = new MigrationManager($this->pdo);
-        $nextMigrationName = $migrationManager->getNextMigrationClass();
+        $migrationManager = new Manager($this->databaseName, $this->pdo);
+        $nextMigrationName = $migrationManager->getNextMigration();
+        
         $this->migrations = new $nextMigrationName;
     }
 
     protected function connectMRollback(): void
     {
         $this->connect();
-        $migrationManager = new MigrationManager($this->pdo);
-        $previousMigrationName = $migrationManager->getPreviouseMigrationClass();
-        $this->migrations = new $previousMigrationName;
+        $migrationManager = new Manager($this->databaseName, $this->pdo);
+        
+        $this->migrations = $migrationManager->getPreviousMigration();
     }
 
-    private function connect(): void
+    protected function connect(): void
     {
         $this->pdo = new PDO(
             sprintf("mysql:host=%s;dbname=%s", getenv('DB_ENDPOINTSCATALOG_HOST'), getenv('DB_ENDPOINTSCATALOG_NAME')),
             getenv('DB_ENDPOINTSCATALOG_USER'),
             getenv('DB_ENDPOINTSCATALOG_PASSWORD')
         );
+
+        $this->databaseName = getenv('DB_ENDPOINTSCATALOG_NAME');
     }
 }
